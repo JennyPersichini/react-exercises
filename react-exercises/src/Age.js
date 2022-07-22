e048062e343771edef507d34c9aac4b95f1c6874
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-07-22 14:42:40 +0200

    complete conditional-rendering-01

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -5,7 +5,9 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 export default class Age extends React.Component {
     render() {
         return ( 
-            <p>Your age is {this.props.age}</p>
+            <div>
+                {this.props.age >= 18 && <p>Your age is {this.props.age}</p>}
+            </div> 
         )
     }
 
