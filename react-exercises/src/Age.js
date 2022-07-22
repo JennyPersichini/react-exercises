0847b7144a0d28c3936f086c092488d4142e9c0b
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-07-22 15:14:22 +0200

    complete conditional-rendering-05

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -6,15 +6,13 @@ export default class Age extends React.Component {
   render() {
     return (
       <div>
-        {this.props.age >= 18 && this.props.age <= 65 ? (
-          <p>Your age is {this.props.age}</p>
-        ) : undefined}
+        {this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>}
       </div>
     );
   }
 
   static defaultProps = {
-    age: "27",
+    age: "5",
   };
 }
 
