22b122e284447e60c67970262a8fd56c0fc7e2aa
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-07-29 12:37:54 +0200

    Complete forms 03

--- react-exercises/src/Login.js
+++ react-exercises/src/Login.js
@@ -21,12 +21,18 @@ export default class Login extends React.Component {
         this.setState({remember: event.target.checked})
     }
 
+    loginButtonClick = () => {
+        this.props.onLogin(this.state)
+    }
+
     render() {
         return (
             <div>
                 <input name='username' placeholder='username' value={this.state.username} onChange={this.handleUsernameInputChange}/>
                 <input name='password' placeholder='password' type='password' value={this.state.password} onChange={this.handlePasswordInputChange}/>
                 <input name='remember' type='checkbox' checked={this.state.remember} onChange={this.handleRememberInputChange}/>
+                
+                <button data-testid="login" disabled = {!this.state.username || !this.state.password} onClick={this.loginButtonClick}>LogIn!!</button>
             </div>
         )
     }
