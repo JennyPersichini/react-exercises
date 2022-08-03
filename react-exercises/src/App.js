e41fd090bbffbc1c6feb47834935e1aa071f043a
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-03 15:22:29 +0200

    correzione branch

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ClickCounter from './ClickCounter'
-import ClickTracker from './ClickTracker'
-import InteractiveWelcome from './InteractiveWelcome'
-import Login from './Login'
-import TodoList from './ToDoList'
-import UncontrolledLogin from './UncontrolledLogin'
+import React from "react";
+import ClickCounter from "./ClickCounter";
+import ClickTracker from "./ClickTracker";
+import Container from "./Container";
+import InteractiveWelcome from "./InteractiveWelcome";
+import Login from "./Login";
+import TodoList from "./ToDoList";
+import UncontrolledLogin from "./UncontrolledLogin";
 
 export default class App extends React.Component {
     render() {
