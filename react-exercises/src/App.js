2d81a70961f7cdd0b4e8ea50d29cb48253d1549d
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-05 07:59:25 +0200

    complete_context-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -3,14 +3,32 @@ import ClickCounter from "./ClickCounter";
 import ClickTracker from "./ClickTracker";
 import Container from "./Container";
 import InteractiveWelcome from "./InteractiveWelcome";
+import { LenguageContext } from "./LanguageContext";
 import Login from "./Login";
 import TodoList from "./ToDoList";
 import UncontrolledLogin from "./UncontrolledLogin";
 
 export default class App extends React.Component {
+  
+state = {
+  language: 'en'
+}
+
+handleLenguageChange = (event) => {
+  this.setState({
+      language: event.target.value
+  })
+}
   render() {
     return (
       <>
+      <select value={this.state.language} onChange={this.handleLenguageChange}>
+        <option value='en'>English</option>
+        <option value='it'>Italiano</option>
+      </select>
+      <LenguageContext.Provider value={this.state.language}>
+
+     
         <Container title="TITOLO">
           <ClickCounter initialValue={50} incrementBy={2} />
           <ClickTracker />
@@ -44,6 +62,7 @@ export default class App extends React.Component {
           />
           <Login />
         </Container>
+        </LenguageContext.Provider>
       </>
     );
   }
