19a3a414143cae276f61351ae67856e3e98d0694
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-16 15:03:02 +0200

    corretto

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
 export default class Age extends React.Component {
     render() {
-        return ( 
-            <div>
-                 {this.props.age !== undefined ? <p>Your age is {this.props.age}</p> : undefined}
-            </div> 
-        )
+      return (
+          <div>
+          <p>Your age is {this.props.age >= 18 && this.props.age <= 65 && <p>{this.props.age}</p> }</p>
+          </div>
+          ) 
     }
-}
+  }
 
     /* static defaultProps = {
         age: '27',
-      }; 
+      }; */
  
- root.render(<Age />);   */
