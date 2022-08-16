deb7bdcbd2e72725b64b34b1c887ef0c42df1990
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-16 14:53:02 +0200

    corretto

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -4,17 +4,15 @@ import ReactDOM from "react-dom";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 export default class Age extends React.Component {
     render() {
-        return ( 
-            <div>
-                {this.props.age >= 18 && <p>Your age is {this.props.age}</p>}
-            </div> 
-        )
+        return <p>Your age is {this.props.age}</p>
     }
+    
 
     static defaultProps = {
         age: '27',
       };
-}
+    }
+
 
 
 root.render(<Age />);
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Age from './Age';
+import Age from "./Age";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 export default class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p> Welcome, <strong>{this.props.name}</strong> !</p>
-        <Age />
+        <p>
+          {" "}
+          Welcome, <strong>{this.props.name}</strong> !
+        </p>
+        <Age age={this.props.age >= 18 && <p>{this.props.age}</p>} />
       </div>
     );
   }
