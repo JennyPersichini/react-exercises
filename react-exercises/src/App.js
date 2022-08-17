19de42c05e3ee4b38152f51f34d581a644ed7d9d
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-17 16:14:48 +0200

    complete react_router02

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Welcome } from './Welcome';
 import { Route, Routes } from 'react-router-dom';
+import { ClickCounter } from "./ClickCounter";
 
 export class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export class App extends React.Component {
       <>
         <Routes>
           <Route path="/" element={<Welcome name='Jimmy'/>} />
+          <Route path="/counter" element={<ClickCounter />} />
         </Routes>
       </>
     );
--- react-exercises/src/ClickCounter.js
+++ react-exercises/src/ClickCounter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCounter } from './useCounter';
+import { Link } from 'react-router-dom';
 
 export function ClickCounter({ initialValue = 0 }) {
   const { counter, onIncrement, onDecrement, onReset } =
@@ -7,6 +8,7 @@ export function ClickCounter({ initialValue = 0 }) {
 
     return (
       <div>
+        <Link to="/">Go to Welcome</Link>
       <h1>Count : {counter}</h1>
       <button onClick={onIncrement}>Increment</button>
       <button onClick={onDecrement}>Decrement</button>
