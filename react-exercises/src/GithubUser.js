21e0baf4b771df1cb733de8a2622a10c3076ef5b
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-08-18 12:34:27 +0200

    complete swr03

--- react-exercises/src/GithubUser.js
+++ react-exercises/src/GithubUser.js
@@ -3,10 +3,11 @@ import { useSWR } from "swr";
 const fetcher = (url) => fetch(url).then((respons) => response.json())
 
 export function GithubUser() {
-  const { data, error, isLoading, } = useSWR(`https://api.github.com/users`, fetcher)
+  const { data, error, isLoading, onRefresh } = useSWR(`https://api.github.com/users`, fetcher)
 
   return (
     <div>
+      <button onClick={onRefresh}>Refresh</button>
       {isLoading && <h3>Loading...</h3>}
       {error && <h3>An error has occurred</h3>}
       {data && !error && (
